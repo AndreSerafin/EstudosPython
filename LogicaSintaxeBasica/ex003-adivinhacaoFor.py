d0cd0cbe79cc751a951213a7bc9e7219d69c8b36
AndreSerafin <andreserafim11@gmail.com>
2022-08-17 22:44:00 -0300

    4

--- LogicaSintaxeBasica/ex003-adivinhacaoFor.py
+++ LogicaSintaxeBasica/ex003-adivinhacaoFor.py
@@ -3,12 +3,25 @@ print("----------------------------------")
 print("Bem vindo ao jogo de adivinhação!")
 print("----------------------------------")
 
-numero_secreto = random.randint(1,100)
-max_tentativas = 15
+numero_secreto = random.randint(1,101)
+
+print("Qual nivel de dificuldade: ")
+print("(1) Facil (2) Medio (3) Dificil")
+
+nivel = int(input("Selecione o nivel: "))
+
+match nivel:
+    case 1:
+        max_tentativas = 15
+    case 2:
+        max_tentativas = 9
+    case 3:
+        max_tentativas = 5
+
+pontuacao_max = 1000
 
 for rodada in range(1,max_tentativas + 1):
     chute = int(input("Digite um número entre 1 e 100: "))
-    
     if (chute >= 1 and chute <= 100):
         print(f"Tentativa {rodada} de {max_tentativas}")
 
@@ -17,8 +30,12 @@ for rodada in range(1,max_tentativas + 1):
             break
         elif(chute > numero_secreto):
             print("Voce errou! O seu chute foi maior do que o numero secreto")
+            pontuacao_max = chute - numero_secreto
+            print("\nPontos:",pontuacao_max)
         elif(chute < numero_secreto):
             print("Voce errou! O seu chute foi menor do que o numero secreto")
+            pontuacao_max -= numero_secreto - chute
+            print("\nPontos:",pontuacao_max)
     else:
         print("Chute fora do intervalo esperado")
         continue
