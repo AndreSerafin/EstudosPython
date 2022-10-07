405483c1765d13af0c53ad63c1d2d7880d237d36
AndreSerafin <andreserafim11@gmail.com>
2022-10-07 11:49:58 -0300

    validador de cpf

--- LogicaSintaxeBasica/ex010DesafioValideCPF.py
+++ LogicaSintaxeBasica/ex010DesafioValideCPF.py
@@ -1,5 +1,32 @@
+print("*******-Verificador de CPF-*******")
 chars = ".-"
-cpf = "021.133.461-84".translate(str.maketrans('','',chars))
+cpf = input("Digite um cpf: ").translate(str.maketrans('','',chars))
+cpf_aux = cpf[:9]
+lista_multipicados = []
 
-for i in cpf:
-    print(i)
+for i,j in zip(cpf_aux, range(10,1,-1)):
+    lista_multipicados.append(int(i)*j)
+
+soma = sum(lista_multipicados)
+
+dig1 = 11 - (soma % 11)
+if dig1 > 9:
+    dig1 = 0
+
+cpf_aux += str(dig1)
+
+lista_multipicados = []
+for i,j in zip(cpf_aux, range(11,1,-1)):
+    lista_multipicados.append(int(i)*j)
+
+soma = sum(lista_multipicados)
+dig2 = 11 - (soma % 11)
+if dig2 > 9:
+    dig2 = 0
+
+cpf_aux += str(dig2)
+
+if cpf_aux == cpf:
+    print("O cpf é valido!")
+else:
+    print("O cpf não é valido!")
